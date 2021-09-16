517987ae981a770ae8b042ef3fc869847118b39d
mathias234 <mathiasda98@gmail.com>
2021-09-16 15:11:31 +0200

    Add controller & views for space

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   end
 
   resources 'facilities'
+  resources 'spaces'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
