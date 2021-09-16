e03d92c531bd5c3ed279f1fa540278d2649fc075
mathias234 <mathiasda98@gmail.com>
2021-09-16 15:45:27 +0200

    Add controller & views for reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.all
+    @review = Review.new
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def create
+    @review = Review.create!(review_params)
+
+    redirect_to reviews_path
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @review = Review.find(params[:id])
+
+    if @review.update(review_params)
+      redirect_to reviews_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :comment, :price, :star_rating, :user_id, :space_id)
+  end
+end
