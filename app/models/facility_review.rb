889070ef6f6494f28d1788cfc6b23ac781ddefb2
mathias234 <mathiasda98@gmail.com>
2021-09-21 14:42:12 +0200

    Add facility review model

---
+++ app/models/facility_review.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class FacilityReview < ApplicationRecord
+  enum experience: { was_allowed: 0, was_allowed_but_bad: 1, was_not_allowed: 2, was_not_available: 3 }
+
+  belongs_to :facility
+  belongs_to :space
+  belongs_to :user
+  belongs_to :review, optional: true
+
+  after_save { space.aggregate_facility_reviews }
+end
--- app/models/space.rb
+++ app/models/space.rb
@@ -4,6 +4,7 @@ class Space < ApplicationRecord
   has_paper_trail
 
   has_many :facilities, dependent: :restrict_with_exception
+  has_many :facility_reviews, dependent: :restrict_with_exception
   has_many :reviews, dependent: :restrict_with_exception
   belongs_to :space_owner
   belongs_to :space_type
