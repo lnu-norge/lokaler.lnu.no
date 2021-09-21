914e9f7ea7eacd17f29da4c86095c63eb57e793e
mathias234 <mathiasda98@gmail.com>
2021-09-21 11:47:06 +0200

    Rename Organization to SpaceOwner

--- app/models/space.rb
+++ app/models/space.rb
@@ -5,6 +5,6 @@ class Space < ApplicationRecord
 
   has_many :facilities, dependent: :restrict_with_exception
   has_many :reviews, dependent: :restrict_with_exception
-  belongs_to :organization
+  belongs_to :space_owner
   belongs_to :space_type
 end
