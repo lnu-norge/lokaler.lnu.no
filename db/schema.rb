c014c76f8b76f6aca053537fb0303c041f148f62
mathias234 <mathiasda98@gmail.com>
2021-09-22 11:13:07 +0200

    Add image upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2021_09_21_103923) do
+=======
+ActiveRecord::Schema.define(version: 2021_09_22_082655) do
+>>>>>>> Add image upload
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+<<<<<<< HEAD
+=======
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+>>>>>>> Add image upload
   create_table "aggregated_facility_reviews", force: :cascade do |t|
     t.bigint "facility_id", null: false
     t.bigint "space_id", null: false
@@ -109,6 +144,11 @@ ActiveRecord::Schema.define(version: 2021_09_21_103923) do
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
+<<<<<<< HEAD
+=======
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+>>>>>>> Add image upload
   add_foreign_key "aggregated_facility_reviews", "facilities"
   add_foreign_key "aggregated_facility_reviews", "spaces"
   add_foreign_key "facilities", "spaces"
