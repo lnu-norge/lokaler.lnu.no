032d9a0980b82ed8c8418e50efd57e60e442d511
mathias234 <mathiasda98@gmail.com>
2021-09-23 10:08:21 +0200

    Fix sum issue with star rating service

---
+++ spec/services/spaces/aggregate_star_rating_service_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Spaces::AggregateStarRatingService do
+  let(:space) { Fabricate(:space) }
+
+  it 'rating equal 4' do
+    Fabricate(:review, space: space, star_rating: 3)
+    Fabricate(:review, space: space, star_rating: 5)
+    Fabricate(:review, space: space, star_rating: 5)
+    Fabricate(:review, space: space, star_rating: 4)
+
+    expect(space.reload.star_rating).to eq(4)
+  end
+end
