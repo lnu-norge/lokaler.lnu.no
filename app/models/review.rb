755dd25e8babb31eb0b75836bb2215edba93829e
mathias234 <mathiasda98@gmail.com>
2021-09-23 10:16:41 +0200

    Add validations for star_rating to be between 1 and 5

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,7 +4,7 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :space
 
-  validates :star_rating, presence: true
+  validates :star_rating, presence: true, numericality: { greater_than: 0, less_than: 6 }
 
   after_save { space.aggregate_star_rating }
 end
--- spec/fabricators/review_fabricator.rb
+++ spec/fabricators/review_fabricator.rb
@@ -4,7 +4,7 @@ Fabricator(:review) do
   title { Faker::Lorem.sentences(number: 1) }
   comment { Faker::Lorem.sentences }
   price { Faker::Number.number(digits: 3) }
-  star_rating { Faker::Number.between(from: 0, to: 5) }
+  star_rating { Faker::Number.between(from: 1, to: 5) }
   user
   space
 end
