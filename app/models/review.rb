4db3cf15c17d5d33cfbfd45dfd7695caa60fe489
Daniel Jackson <daniel@snilldesign.no>
2021-09-24 10:33:46 +0200

    Added after_destroy to Review

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,4 +7,5 @@ class Review < ApplicationRecord
   validates :star_rating, presence: true, numericality: { greater_than: 0, less_than: 6 }
 
   after_save { space.aggregate_star_rating }
+  after_destroy { space.aggregate_star_rating }
 end
