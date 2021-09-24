2ce53bec03a9add44a0ce787b01c41f3402754ec
mathias234 <mathiasda98@gmail.com>
2021-09-24 08:18:39 +0200

    Change to using nil to represent no star_rating and change star_rating to decimal

--- app/services/spaces/aggregate_star_rating_service.rb
+++ app/services/spaces/aggregate_star_rating_service.rb
@@ -8,6 +8,8 @@ module Spaces
     end
 
     def call
+      space.update!(star_rating: nil) if space.reviews.count.zero?
+
       star_rating = space.reviews.all.sum(&:star_rating)
 
       space.update!(star_rating: star_rating / space.reviews.count)
--- db/migrate/20210923070806_add_star_rating_to_space.rb
+++ db/migrate/20210923070806_add_star_rating_to_space.rb
@@ -1,5 +1,5 @@
 class AddStarRatingToSpace < ActiveRecord::Migration[6.1]
   def change
-    add_column :spaces, :star_rating, :integer, null: false, default: 0
+    add_column :spaces, :star_rating, :decimal, null: true
   end
 end
