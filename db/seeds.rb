72e5949e9fa5cd91780584950ee836bd352798fd
Daniel Jackson <daniel@snilldesign.no>
2021-09-28 17:46:29 +0200

    Added seed structure for each env. and some test seeds for development.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Seeds are split into different files, based on environment:
+ActiveRecord::Base.transaction do
+  ['common', Rails.env].each do |seedfile|
+    seed_file = Rails.root.join "/db/seeds/#{seedfile}.rb"
+    next unless File.exist?(seed_file)
+
+    # rubocop:disable  Rails/Output
+    puts "- - Seeding data from file: #{seedfile}"
+    # rubocop:enable  Rails/Output
+    require seed_file
+  end
+end
---
+++ db/seeds/spaces/nabbetorp_no_booking.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+## Space Owner
+viken = SpaceOwner.create(
+  orgnr: '921693230'
+)
+
+## Space Type
+barneskole = SpaceType.create(
+  type_name: '1. til 7.'
+)
+
+## Rich text fields
+how_to_book = '<p>Nabbetorp skole er en
+spesialskole og er ikke åpen for leie eller
+lån. De ønsker ikke bli kontaktet.</p>'
+
+## Create space
+Space.create(
+  title: 'Nabbetorp spesialskole',
+  address: 'Enggata 76',
+  post_number: '1636',
+  post_address: 'Gamle Fredrikstad',
+  lat: 59.213868,
+  lng: 10.972267,
+  space_owner_id: viken.id,
+  space_type_id: barneskole.id,
+  organization_number: '974766337',
+  municipality_code: '3004',
+  fits_people: nil,
+  star_rating: nil,
+  how_to_book: how_to_book
+  # open_for_being_contacted: false
+  # TODO: Add this to the model!
+)
