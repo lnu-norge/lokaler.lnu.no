e23fbcfa14973656272d22720c6ba0ef3b4202fc
mathias234 <mathiasda98@gmail.com>
2021-09-29 09:35:58 +0200

    Get map markers from backend

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -9,20 +9,27 @@ export default class extends Controller {
       container: 'map-frame',
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [9.610, 59.178],
-      zoom: 15
+      zoom: 9
     });
 
+    /*
     this.map.on('moveend', () => {
       const center = this.map.getBounds().getCenter()
       this.addMarker({lat: center.lat, lng: center.lng, text: "Test Marker"})
     });
+    */
 
+    const markers = JSON.parse(this.element.dataset.markers)
+
+    markers.forEach(marker => {
+      this.addMarker(marker)
+    });
   }
 
   addMarker(marker) {
     const element = document.createElement('div')
-    element.className = "rounded p-1 border border-black bg-white"
-    element.innerText = marker.text
+    element.className = "rounded-md py-1 px-2 border border-gray-400 bg-white shadow-lg"
+    element.innerText = marker.title
     new mapboxgl.Marker(element)
       .setLngLat([marker.lng, marker.lat])
       .addTo(this.map)
