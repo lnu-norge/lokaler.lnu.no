5466df3a15d71c8a77189a6cfba60b090cadb5fa
Daniel Jackson <daniel@snilldesign.no>
2021-09-29 07:57:01 +0200

    Update seed readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,19 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database with its default values.
+# Seed files contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+# You will find seed files for each environment in the db/seeds folder
+# They are loaded conditionally on the development environment here.
+#
+# Seeds that are common for all environment are in the "seeds/common.rb" file.
 
-# Seeds are split into different files, based on environment:
+# Load seeds based on environment::
 ActiveRecord::Base.transaction do
   ['common', Rails.env].each do |possible_seed_file|
     file = Rails.root.join('db', 'seeds', "#{possible_seed_file}.rb")
