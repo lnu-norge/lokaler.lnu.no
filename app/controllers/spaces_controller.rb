600f2fecb01824916dc16cdc04c6ad772df4f299
Daniel Jackson <daniel@snilldesign.no>
2021-09-30 13:11:03 +0200

    Keep marker state unless it changes, and update styling of map markers

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -48,7 +48,13 @@ class SpacesController < ApplicationController
     )
 
     response = spaces.map do |space|
-      { lat: space.lat, lng: space.lng, title: space.title }
+      {
+        lat: space.lat,
+        lng: space.lng,
+        id: space.id,
+        title: space.title,
+        starRating: space.star_rating
+      }
     end
 
     render json: response
