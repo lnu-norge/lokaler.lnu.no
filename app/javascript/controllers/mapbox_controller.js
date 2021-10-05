cdd1e1f15f2aa513c5d84792d41ed75340ca4124
mathias234 <mathiasda98@gmail.com>
2021-10-05 13:29:03 +0200

    Add overcommit EsLint for app/javascript/controllers

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -1,5 +1,5 @@
-import mapboxgl from "mapbox-gl"
-import { Controller } from "stimulus"
+import mapboxgl from 'mapbox-gl';
+import { Controller } from 'stimulus';
 
 export default class extends Controller {
   initialize() {
@@ -9,62 +9,62 @@ export default class extends Controller {
       container: 'map-frame',
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [10.925860, 59.223840],
-      zoom: 11
+      zoom: 11,
     });
 
     this.map.on('moveend', () => {
-      this.loadNewMapPosition()
+      this.loadNewMapPosition();
     });
 
     // Hash for storing markers, based on
     // { spaceId: mapBoxMarker }
-    this.markers = {}
+    this.markers = {};
 
-    this.loadNewMapPosition()
+    this.loadNewMapPosition();
   }
 
   addMarker(space) {
     // If marker is already added, then just update that element, otherwise, create a new one
     const element = this.markers[space.id]
       ? this.markers[space.id].getElement()
-      : document.createElement('div')
+      : document.createElement('div');
 
-    element.innerHTML = space.html
+    element.innerHTML = space.html;
 
     if (!this.markers[space.id]) {
       // Need to add new markers to this.markers, to keep track:
       this.markers[space.id] = new mapboxgl.Marker(element)
         .setLngLat([space.lng, space.lat])
-        .addTo(this.map)
+        .addTo(this.map);
     }
   }
 
   removeMarker(key) {
-    this.markers[key].remove()
-    delete this.markers[key]
+    this.markers[key].remove();
+    delete this.markers[key];
   }
 
   async loadNewMapPosition() {
-    document.getElementById("space-listing").innerText = "Laster..."
+    document.getElementById('space-listing').innerText = 'Laster...';
 
-    const northWest = this.map.getBounds().getNorthWest()
-    const southEast = this.map.getBounds().getSouthEast()
-    const fetchSpacesInRectUrl = `/spaces_in_rect?north_west_lat=${northWest.lat}&north_west_lng=${northWest.lng}&south_east_lat=${southEast.lat}&south_east_lng=${southEast.lng}`
-    const spacesInRect = await (await fetch(fetchSpacesInRectUrl)).json()
+    const northWest = this.map.getBounds().getNorthWest();
+    const southEast = this.map.getBounds().getSouthEast();
+    const fetchSpacesInRectUrl = `/spaces_in_rect?north_west_lat=${northWest.lat}&north_west_lng=${northWest.lng}&south_east_lat=${southEast.lat}&south_east_lng=${southEast.lng}`;
+    const spacesInRect = await (await fetch(fetchSpacesInRectUrl)).json();
 
     // Replace the spaces list with the new view rendered by the server
-    document.getElementById("space-listing").innerHTML = spacesInRect.listing
+    document.getElementById('space-listing').innerHTML = spacesInRect.listing;
 
-    const markers = spacesInRect.markers
+    const { markers } = spacesInRect;
     // Remove markers that are no longer relevant
-    Object.keys(this.markers).forEach(key => {
-      if (markers.find(space => space.id === key)) return;
-      this.removeMarker(key)
-    })
+    Object.keys(this.markers).forEach((key) => {
+      if (markers.find((space) => space.id === key)) return;
+      this.removeMarker(key);
+    });
 
     // Add or update the ones we want to show:
-    markers.forEach(space => {
-      this.addMarker(space)
+    markers.forEach((space) => {
+      this.addMarker(space);
     });
   }
 }
