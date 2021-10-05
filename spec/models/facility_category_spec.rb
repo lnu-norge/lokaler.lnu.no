cabc2572e934f80d5065a147e2fff4150009a829
Daniel Jackson <daniel@snilldesign.no>
2021-10-05 14:29:33 +0200

    Trying to fix tests on github

--- spec/models/facility_category_spec.rb
+++ spec/models/facility_category_spec.rb
@@ -4,4 +4,8 @@ require 'rails_helper'
 
 RSpec.describe FacilityCategory, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'can create a facility category' do
+    expect(Fabricate(:facility_category)).to be_truthy
+  end
 end
