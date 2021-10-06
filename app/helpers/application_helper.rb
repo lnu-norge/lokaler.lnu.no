830ac43a7f415a112a2dd7da251832d8b120411a
Anders Solvang <anders@rubynor.com>
2021-10-06 09:02:47 +0200

    Add password reset and registration edit views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,8 @@
 module ApplicationHelper
   def helper_flash_type(msg_type, message)
     case msg_type
-    when 'successs'
-      render partial: 'shared/success', locals: { message: message }
+    when 'notice'
+      render partial: 'shared/notice', locals: { message: message }
     when 'alert'
       render partial: 'shared/alert', locals: { message: message }
     end
