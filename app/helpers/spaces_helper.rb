717dc95c10118988ae1f1d014ff5600a6ff0b124
Daniel Jackson <daniel@snilldesign.no>
2021-10-07 14:37:51 +0200

    Move edit field and edit inline partials into edit folder

--- app/helpers/spaces_helper.rb
+++ app/helpers/spaces_helper.rb
@@ -6,7 +6,7 @@ module SpacesHelper
   end
 
   def inline_editable(field, title = { h2: nil }, &block)
-    render partial: 'spaces/editable_inline', locals: {
+    render partial: 'spaces/edit/editable_inline', locals: {
       field: field,
       title_tag: title.first[0],
       title_text: title.first[1],
