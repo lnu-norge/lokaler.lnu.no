b2568d77f0d60f4032cb099b1decd3a3764c2197
mathias234 <mathiasda98@gmail.com>
2021-10-07 10:41:09 +0200

    Enable assets.compile
    This is not a great solution and can negativly impact performance, but
    due to some issues that we were unable to solve with inline_svg I went
    with this for now.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,10 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # NOTE: This is not optimal but we had to enable it for inline_svg
+  # possibly look into a fix for this
+  # ref: https://devcenter.heroku.com/articles/rails-asset-pipeline#compile-set-to-true-in-production
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
