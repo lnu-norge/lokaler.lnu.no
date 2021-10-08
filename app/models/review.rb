715f228057fad9adfbf284e1f8b27e413b28e455
Daniel Jackson <daniel@snilldesign.no>
2021-10-08 16:33:48 +0200

    Add some tests for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,6 +4,8 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :space
 
+  has_many :facility_reviews, dependent: :destroy
+
   validates :star_rating, numericality: { greater_than: 0, less_than: 6 }, allow_nil: true
 
   after_save { space.aggregate_star_rating }
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,5 +3,41 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'can add a review' do
+    review = Fabricate(:review)
+    Fabricate(:facility_review, review: review)
+    expect(review.title).to be_truthy
+    expect(review.space).to be_truthy
+    expect(review.comment).to be_truthy
+    expect(review.price).to be_truthy
+    expect(review.star_rating).to be_truthy
+    expect(review.user).to be_truthy
+    expect(review.facility_reviews.count).to be 1
+  end
+
+  it 'sets stars and facility_reviews for a Space when adding and removing a review' do
+    space = Fabricate(:space)
+    review = Fabricate(:review, space: space)
+    facility = Fabricate(:facility)
+    facility_review = Fabricate(
+      :facility_review,
+      review: review,
+      facility: facility,
+      space: space,
+      experience: :was_not_allowed
+    )
+
+    space.reload
+
+    expect(space.star_rating).to eq(review.star_rating)
+    expect(space.reviews_for_facility(facility)).to eq('unlikely')
+
+    review.destroy
+    space.reload
+
+    expect(space.star_rating).to be_nil
+    expect(space.reviews_for_facility(facility)).to eq('unknown')
+    expect { review.reload }.to raise_error(ActiveRecord::RecordNotFound)
+    expect { facility_review.reload }.to raise_error(ActiveRecord::RecordNotFound)
+  end
 end
