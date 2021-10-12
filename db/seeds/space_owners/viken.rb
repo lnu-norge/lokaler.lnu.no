d31f95c5e5907e7cbc435597a6c05e68ad761d1e
Daniel Jackson <daniel@snilldesign.no>
2021-10-12 16:04:59 +0200

    Add new space owner model to fabricators, seeds, and tests

--- db/seeds/space_owners/viken.rb
+++ db/seeds/space_owners/viken.rb
@@ -1,23 +1,33 @@
 # frozen_string_literal: true
 
-viken = SpaceOwner.create_or_find_by(
-  title: 'Viken fylkeskommune',
-  orgnr: '921693230'
-)
-viken.how_to_book = '<p>Viken fylkeskommune krever at du annekterer
-et nabofylke før du kan booke.</p>
-<p>Når annekteringen er ferdig, så fyller du ut <a href="#">skjema A-123</a> og
-leverer til statsministerens kontor, så vil bli kontaktet./p>',
-                    viken.pricing = "<p>Lokale organisasjoner kan få det gratis.</p>
-<p>Andre må betale for vask og vakthold, etter prislisten:
-<a href='#'>Last ned prisliste for Viken (PDF)</a></p>"
-viken.terms = '<ul>
-  <li><a href="#">Vlikår for overnatting i Viken (PDF)</a></li>
-  <li><a href="#">Brannrutiner for overnatting i Viken (PDF)</a></li>
-  <li><a href="#">Nattevaktskjema for overnatting i Viken (PDF)</a></li>
-</ul>'
-viken.who_can_use = '<p>Alle frivillige organisasjoner
-kan ta kontakt. Barne- og ungdoms- organisasjoner
-og organisasjoner med tilknytning til Viken
- prioriteres.<br /><a href="#">Last ned Vikens
-veileder (PDF)</a></p>'
+def create_viken # rubocop:disable Metrics/MethodLength
+  how_to_book = '<p>Viken fylkeskommune krever at du annekterer
+    et nabofylke før du kan booke.</p>
+    <p>Når annekteringen er ferdig, så fyller du ut <a href="#">skjema A-123</a> og
+    leverer til statsministerens kontor, så vil bli kontaktet.</p>'
+
+  pricing = "<p>Lokale organisasjoner kan få det gratis.</p>
+    <p>Andre må betale for vask og vakthold, etter prislisten:
+    <a href='#'>Last ned prisliste for Viken (PDF)</a></p>"
+
+  terms = '<ul>
+      <li><a href="#">Vlikår for overnatting i Viken (PDF)</a></li>
+      <li><a href="#">Brannrutiner for overnatting i Viken (PDF)</a></li>
+      <li><a href="#">Nattevaktskjema for overnatting i Viken (PDF)</a></li>
+    </ul>'
+
+  who_can_use = '<p>Alle frivillige organisasjoner
+    kan ta kontakt. Barne- og ungdoms- organisasjoner
+    og organisasjoner med tilknytning til Viken
+     prioriteres.<br /><a href="#">Last ned Vikens
+    veileder (PDF)</a></p>'
+
+  SpaceOwner.create(
+    title: 'Viken fylkeskommune',
+    orgnr: '921693230',
+    how_to_book: how_to_book,
+    pricing: pricing,
+    terms: terms,
+    who_can_use: who_can_use
+  )
+end
