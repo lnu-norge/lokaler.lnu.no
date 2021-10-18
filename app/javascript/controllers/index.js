3ad5ee7a64310e51a996e81978e2738b05b2e515
mathias234 <mathiasda98@gmail.com>
2021-10-18 16:02:39 +0200

    Show all location options to the user, using tom-select

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,8 @@
 import { Application } from 'stimulus';
 import { definitionsFromContext } from 'stimulus/webpack-helpers';
 
+require("../../javascript/stylesheets/tom-select.css")
+
 const application = Application.start();
 const context = require.context('controllers', true, /_controller\.js$/);
 application.load(definitionsFromContext(context));
