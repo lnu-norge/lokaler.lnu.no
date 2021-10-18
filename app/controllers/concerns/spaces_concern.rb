63ee21f034a85010b0281fc33adad71932c68b67
mathias234 <mathiasda98@gmail.com>
2021-10-18 14:26:44 +0200

    Order results based on best match

--- app/controllers/concerns/spaces_concern.rb
+++ app/controllers/concerns/spaces_concern.rb
@@ -14,12 +14,19 @@ module SpacesConcern
   def filter_on_facilities(spaces, facilities) # rubocop:disable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
     return spaces if facilities.nil?
 
-    spaces.reject do |space|
-      space.aggregated_facility_reviews.select do |review|
-        if facilities.include?(review.facility_id.to_s) && (review.unknown? || review.impossible? || review.unlikely?)
-          review
+    result = spaces.map do |space|
+      match_count = 0
+      space.aggregated_facility_reviews.each do |review|
+        if facilities.include?(review.facility_id.to_s) && (review.unknown? || review.maybe? || review.likely?)
+          match_count += 1
         end
-      end.empty?
+      end
+
+      next if match_count.zero?
+
+      OpenStruct.new(match_count: match_count, space: space)
     end
+
+    result.sort_by(&:match_count).map(&:space)
   end
 end
