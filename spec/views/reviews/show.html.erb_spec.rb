681d3091edc6cfb75a2cfb5cedb191bb0225cdb8
Anders Solvang <anders@rubynor.com>
2021-10-21 19:16:27 +0200

    Add view tests for our views

---
+++ spec/views/reviews/show.html.erb_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'reviews/show.html.erb', type: :view do
+  let(:review) { Fabricate :review }
+
+  it 'renders the page' do
+    assign(:review, review)
+    render
+
+    expect(rendered).to match /h1/
+    expect(rendered).to match /#{review.title}/
+    expect(rendered).to match /#{review.comment}/
+    expect(rendered).to match /#{review.price}/
+    expect(rendered).to match /#{review.star_rating}/
+  end
+end
