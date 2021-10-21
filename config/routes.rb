8479f15c95f7deab72b5f15b69808f57bea98092
Anders Solvang <anders@rubynor.com>
2021-10-21 19:15:16 +0200

    Remove unused routes and add heading tags to pages that were missing them

--- config/routes.rb
+++ config/routes.rb
@@ -17,14 +17,17 @@ Rails.application.routes.draw do
 
   root to: 'devise/sessions#new', as: 'unauthenticated_root'
 
-  resources 'facilities'
-  resources 'spaces'
-  get '/spaces/:id/edit/:field', to: 'spaces#edit_field', as: 'edit_field'
-  resources 'space_owners'
+  # Resources
+  resources 'facilities', excep: 'new'
+  resources 'space_owners', except: 'new'
   resources 'reviews'
 
+  # Spaces routes
+  resources 'spaces', except: 'new'
+  get '/spaces/:id/edit/:field', to: 'spaces#edit_field', as: 'edit_field'
   get 'spaces_in_rect', to: 'spaces#spaces_in_rect'
   get 'rect_for_spaces', to: 'spaces#rect_for_spaces'
   post 'spaces/upload_image', to: 'spaces#upload_image'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
