3a012007dfacbffac90e212bb76ab196e5f96ce5
Anders Solvang <anders@rubynor.com>
2021-10-21 15:50:04 +0200

    Apply stash

--- spec/models/space_owner_spec.rb
+++ spec/models/space_owner_spec.rb
@@ -3,12 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe SpaceOwner, type: :model do
-  it 'can add a SpaceOwner with all attributes' do
-    space_owner = described_class.create Fabricate.attributes_for(:space_owner)
-    expect(space_owner).to be_valid
-    expect(space_owner.about).to be_truthy
-  end
-
   it 'can create a spaceOwner' do
     expect(Fabricate(:space_owner)).to be_truthy
   end
