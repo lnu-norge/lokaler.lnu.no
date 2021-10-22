8fb2a26ff4161d5cbc1e16a7dafbd8db4ed099ad
Anders Solvang <anders@rubynor.com>
2021-10-22 13:11:39 +0200

    Removed unsused comment and notice

--- app/controllers/authenticate_controller.rb
+++ app/controllers/authenticate_controller.rb
@@ -9,9 +9,7 @@ class AuthenticateController < ApplicationController
     if user_signed_in?
       super
     else
-      redirect_to new_user_session_path, notice: 'if you want to add a notice'
-      ## if you want render 404 page
-      ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
+      redirect_to new_user_session_path
     end
   end
 end
