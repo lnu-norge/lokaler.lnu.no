0624956a6cf417f32f19ae88cabb1ea6782196f9
mathias234 <mathiasda98@gmail.com>
2021-10-25 13:55:57 +0200

    Fix flaky spec

--- spec/views/spaces/show.html.erb_spec.rb
+++ spec/views/spaces/show.html.erb_spec.rb
@@ -3,14 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe 'spaces/show', type: :view do
-  let(:space) { Fabricate :space }
+  let(:space) { Fabricate :space, title: 'Space Title' }
 
   it 'renders the page' do
     assign(:space, space)
     render
 
     expect(rendered).to match /h1/
-    expect(rendered).to match /#{space.title}/
+    expect(rendered).to match /Space Title/
     expect(rendered).to match /space_#{space.id}-how_to_book/
     expect(rendered).to match /space_#{space.id}-who_can_use/
     expect(rendered).to match /space_#{space.id}-pricing/
