9df72544945701193bbcc4022750bc681bccf821
Daniel Jackson <daniel@snilldesign.no>
2021-10-27 10:00:09 +0200

    Add modal helpers, and allow us to create modal views

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,19 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="modal"
 export default class extends Controller {
-  static targets = [ "modal", "background" ]
+  static targets = [ "modal", "background", "closeButton" ]
 
   connect() {
-    console.log('Modal connected')
-    this.modalTarget.onclick = () => {
-      alert("hi")
-    }
     this.backgroundTarget.onclick = (event) => {
-      // Check that we didn't click anything above it:
-      if (event.target !== this.backgroundTarget) {
-        return console.log(event)
-      }
-      alert ("clicked background")
+      // Check that we didn't click anything else it:
+      if (event.target !== this.backgroundTarget) return
+
+      // Then close the modal:
+      this.closeButtonTarget.click()
     }
   }
 }
