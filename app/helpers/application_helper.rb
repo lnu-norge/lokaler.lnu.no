4944f8e86ecd0aaf15e31bbb31dd5e21c3999c99
Daniel Jackson <daniel@snilldesign.no>
2021-10-27 10:00:09 +0200

    Add modal helpers, and allow us to create modal views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,18 @@ module ApplicationHelper
       render partial: "shared/alert", locals: { message: message }
     end
   end
+
+  def modal_link_to(text, id, **options)
+    turbo_frame_tag id do
+      link_to text, id, **options
+    end
+  end
+
+  def modal_content_for(id, parent, &block)
+    render partial: 'shared/modal', locals: {
+      id: id,
+      parent: parent,
+      block: block
+    }
+  end
 end
