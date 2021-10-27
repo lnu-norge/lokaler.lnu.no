dcf089403b5024121bfc0fc0d65869d92accdb79
mathias234 <mathiasda98@gmail.com>
2021-10-27 09:26:22 +0200

    Fix incorrect habtm relation

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Organization < ApplicationRecord
-  has_and_belongs_to_many :organizations
+  has_and_belongs_to_many :users
   has_many :reviews, dependent: :restrict_with_exception
 
   validates :name, presence: true
