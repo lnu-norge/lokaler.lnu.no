941facb3bd07704e5f3f99a825a079fadaa2078e
Daniel Jackson <daniel@snilldesign.no>
2021-10-28 08:37:00 +0200

    Removed allowed_but_bad from FacilityExperience enum

--- app/models/facility_review.rb
+++ app/models/facility_review.rb
@@ -9,7 +9,7 @@ class FacilityReview < ApplicationRecord
   after_save { space.aggregate_facility_reviews }
   after_destroy { space.aggregate_facility_reviews }
 
-  enum experience: { was_allowed: 0, was_allowed_but_bad: 1, was_not_allowed: 2, was_not_available: 3 }
+  enum experience: { was_allowed: 0, was_not_allowed: 2, was_not_available: 3 }
 
   scope :impossible, -> { where(experience: :was_not_available) }
   scope :positive, -> { where(experience: [:was_allowed, :was_allowed_but_bad]) }
@@ -19,8 +19,6 @@ class FacilityReview < ApplicationRecord
     case experience
     when "was_allowed"
       :likely
-    when "was_allowed_but_bad"
-      :maybe
     when "was_not_allowed"
       :unlikely
     when "was_not_available"
