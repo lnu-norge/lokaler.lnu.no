621d86ef705b2a36080b472b1d15d3a0eb889a65
Daniel Jackson <daniel@snilldesign.no>
2021-10-28 16:19:41 +0200

    Fix failing tests

--- spec/fabricators/review_fabricator.rb
+++ spec/fabricators/review_fabricator.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Fabricator(:review) do
-  title { Faker::Lorem.words(number: 2).join }
+  title { Faker::Lorem.sentence(word_count: 4) }
   comment { Faker::Lorem.sentences }
   price { Faker::Number.number(digits: 3) }
   star_rating { Faker::Number.between(from: 1, to: 5) }
