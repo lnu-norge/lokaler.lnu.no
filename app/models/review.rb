cc78d25502c9b096be6f49113710d584ed205314
Daniel Jackson <daniel@snilldesign.no>
2021-10-29 20:21:11 +0200

    Improved error handling for missing facility reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -28,6 +28,7 @@ class Review < ApplicationRecord
   validates :price, numericality: { greater_than: 0 }, allow_nil: true
   validates :star_rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }, if: :been_there?
   validates :organization, presence: true
+  validates :facility_reviews, presence: true, if: :only_contacted?
 
   after_save { space.aggregate_star_rating }
   after_destroy { space.aggregate_star_rating }
