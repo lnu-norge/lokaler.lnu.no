40a8f588f5f0715c2f91e59f229b52862c3f0b99
Anders Solvang <anders@rubynor.com>
2021-11-02 20:17:09 +0100

    Update model tests and remove two rubocop warnings

--- spec/models/space_contact_spec.rb
+++ spec/models/space_contact_spec.rb
@@ -3,11 +3,32 @@
 require 'rails_helper'
 
 RSpec.describe SpaceContact, type: :model do
-  it 'can create a spaceContact for a space' do
-    expect(Fabricate(:space_contact)).to be_truthy
+  context 'when creating a new space_contact' do
+    it 'can create a spaceContact for a space' do
+      expect(Fabricate(:space_contact)).to be_truthy
+    end
+
+    it 'can create a spaceContact for a space_owner' do
+      expect(Fabricate(:space_contact, space: nil)).to be_truthy
+    end
   end
 
-  it 'can create a spaceContact for a space_owner' do
-    expect(Fabricate(:space_contact, space: nil)).to be_truthy
+  context 'when updating a space_contact' do
+    let(:space_contact) { Fabricate(:space_contact) }
+    let(:title) { 'new title' }
+
+    it 'can update a spaceContact for a space' do
+      space_contact.update(title: title)
+      expect(described_class.find_by(title: title)).to eq(space_contact)
+    end
+  end
+
+  context 'when deleting a space_contact' do
+    let(:space_contact) { Fabricate(:space_contact, title: 'title') }
+
+    it 'can delete a space contact for a space' do
+      space_contact.destroy
+      expect(described_class.find_by(title: 'title')).to be_nil
+    end
   end
 end
