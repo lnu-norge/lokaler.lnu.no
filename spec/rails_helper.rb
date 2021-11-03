37d0ad131a3f11a38a21a7efca2bb997fab1e05c
Daniel Jackson <daniel@snilldesign.no>
2021-11-03 10:52:06 +0100

    Add test for FacilityReview update

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,7 +67,7 @@ RSpec.configure do |config|
 
   config.include Devise::Test::ControllerHelpers, type: :view
   config.include Devise::Test::ControllerHelpers, type: :controller
-
+  config.include Devise::Test::IntegrationHelpers, type: :request
 
   VCR.configure do |config|
     config.cassette_library_dir = "spec/support/vcr"
