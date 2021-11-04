196e9a57c18eeb5a55edd1a33da59a9d421993c0
Anders Solvang <anders@rubynor.com>
2021-11-04 13:48:32 +0100

    Fix turbostreams to now work as intended

--- app/models/space_contact.rb
+++ app/models/space_contact.rb
@@ -4,9 +4,16 @@ class SpaceContact < ApplicationRecord
   belongs_to :space, optional: true
   belongs_to :space_owner
 
-  after_create_commit { broadcast_prepend_to 'space_contacts' }
-  after_update_commit { broadcast_replace_to 'space_contacts' }
+  after_create_commit { broadcast_prepend_to 'space_contacts', partial: 'space_contacts/space_contact' }
+  after_update_commit { broadcast_replace_to 'space_contacts', partial: 'space_contacts/space_contact' }
   after_destroy_commit { broadcast_remove_to 'space_contacts' }
 
   validates :title, presence: true
+  validate :any_present?
+
+  def any_present?
+    if %w[telephone email url description].all? { |attr| self[attr].blank? } # rubocop:disable Style/GuardClause
+      errors.add :base, I18n.t('space_contact.at_least_one_error_message')
+    end
+  end
 end
