986713676f6d85b8df8969adde16bd930ace7fbe
mathias234 <mathiasda98@gmail.com>
2021-11-04 15:38:50 +0100

    Make it more clear which field was changed

--- config/initializers/paper_trail_streams.rb
+++ config/initializers/paper_trail_streams.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# This extension to paper trail adds a after_create_commit hook,
+# that will broadcast new Version objects to any listening clients.
 module PaperTrail
   class Version < ActiveRecord::Base # rubocop:disable Rails/ApplicationRecord
     include PaperTrail::VersionConcern
