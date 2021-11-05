9b14503d9a2e1491ccc6e01d28421688e18fbffd
Anders Solvang <anders@rubynor.com>
2021-11-05 15:53:37 +0100

    Change space contact to be able to only belong to one or the other, or both

--- app/models/space_contact.rb
+++ app/models/space_contact.rb
@@ -4,18 +4,20 @@ class SpaceContact < ApplicationRecord
   has_paper_trail
 
   belongs_to :space, optional: true
-  belongs_to :space_owner
+  belongs_to :space_owner, optional: true
 
-  after_create_commit { broadcast_prepend_to 'space_contacts', partial: 'space_contacts/space_contact' }
-  after_update_commit { broadcast_replace_to 'space_contacts', partial: 'space_contacts/space_contact' }
-  after_destroy_commit { broadcast_remove_to 'space_contacts' }
+  after_create_commit { broadcast_prepend_to "space_contacts", partial: "space_contacts/space_contact" }
+  after_update_commit { broadcast_replace_to "space_contacts", partial: "space_contacts/space_contact" }
+  after_destroy_commit { broadcast_remove_to "space_contacts" }
 
   validates :title, presence: true
   validate :any_present?
+  validates :space, presence: true, unless: :space_owner
+  validates :space_owner, presence: true, unless: :space
 
   def any_present?
-    if %w[telephone email url description].all? { |attr| self[attr].blank? } # rubocop:disable Style/GuardClause
-      errors.add :base, I18n.t('space_contact.at_least_one_error_message')
-    end
+    return unless %w[telephone email url description].all? { |attr| self[attr].blank? }
+
+    errors.add :base, I18n.t("space_contact.at_least_one_error_message")
   end
 end
