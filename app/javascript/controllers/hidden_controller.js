a9c9a48d742dc2101f0529e499e0033c36d2228e
Anders Solvang <anders@rubynor.com>
2021-11-05 14:49:55 +0100

    Change show hide new contact button

--- app/javascript/controllers/hidden_controller.js
+++ app/javascript/controllers/hidden_controller.js
@@ -2,9 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="hidden"
 export default class extends Controller {
-  static targets = ["hideToggle", "title"]
+  static targets = ["hideToggle", "title", "open", "close"]
 
   toggle() {
+    this.openTarget.classList.toggle("hidden");
+    this.closeTarget.classList.toggle("hidden");
     this.hideToggleTarget.classList.toggle("hidden");
     this.titleTarget.focus();
   }
