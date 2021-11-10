a59b72c336e218a369e44e8a26495d41c0f52496
Daniel Jackson <daniel@snilldesign.no>
2021-11-10 09:25:51 +0100

    Fix error after rebase

--- app/models/facility_review.rb
+++ app/models/facility_review.rb
@@ -12,7 +12,7 @@ class FacilityReview < ApplicationRecord
   enum experience: { was_allowed: 0, was_not_allowed: 2, was_not_available: 3 }
 
   scope :impossible, -> { where(experience: :was_not_available) }
-  scope :positive, -> { where(experience: [:was_allowed, :was_allowed_but_bad]) }
+  scope :positive, -> { where(experience: :was_allowed) }
   scope :negative, -> { where(experience: :was_not_allowed) }
 
   validates :facility, uniqueness: { scope: :review }
