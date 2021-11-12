7818a8cfca2231ac4752383cc577380d65da3966
mathias234 <mathiasda98@gmail.com>
2021-11-12 10:20:57 +0100

    Add default url option to production as env variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,6 +102,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  Rails.application.routes.default_url_options[:host] = ENV["HOST"]
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
