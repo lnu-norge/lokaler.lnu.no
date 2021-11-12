8b85e0dc65110d35eaef2c94e7155f08e318e5f8
mathias234 <mathiasda98@gmail.com>
2021-11-12 11:31:53 +0100

    Add new space owner if what you typed was not found

--- app/javascript/controllers/multiselect_controller.js
+++ app/javascript/controllers/multiselect_controller.js
@@ -2,7 +2,15 @@ import { Controller } from '@hotwired/stimulus'
 import TomSelect from 'tom-select'
 
 export default class extends Controller {
+  static values = {
+    allowNew: Boolean,
+  }
   connect() {
-    this.tomselect = new TomSelect(this.element);
+    this.tomselect = new TomSelect(
+      this.element,
+      {
+        create: this.allowNewValue,
+      }
+    );
   }
 }
