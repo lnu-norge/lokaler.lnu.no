e02a08e0ebf5b9a129d34d5ff96d8f3be870c916
Daniel Jackson <daniel@snilldesign.no>
2021-11-26 14:21:41 +0100

    Style SpaceContact#new as modal

--- app/javascript/controllers/hidden_controller.js
+++ app/javascript/controllers/hidden_controller.js
@@ -5,8 +5,6 @@ export default class extends Controller {
   static targets = ["hideToggle", "title", "open", "close"]
 
   toggle() {
-    this.openTarget.classList.toggle("hidden");
-    this.closeTarget.classList.toggle("hidden");
     this.hideToggleTarget.classList.toggle("hidden");
     this.titleTarget.focus();
   }
