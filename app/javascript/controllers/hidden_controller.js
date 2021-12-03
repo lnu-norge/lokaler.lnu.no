ca8f2f7e4bd7fa90e1941b436d32c7e5bd002273
Daniel Jackson <daniel@snilldesign.no>
2021-12-03 08:55:15 +0100

    Clean up use of hidden toggle controller

--- app/javascript/controllers/hidden_controller.js
+++ app/javascript/controllers/hidden_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="hidden"
 export default class extends Controller {
-  static targets = ["hideToggle", "focus" ]
+  static targets = ["hideToggle", "focus"]
 
   toggle() {
     this.hideToggleTarget.classList.toggle("hidden");
