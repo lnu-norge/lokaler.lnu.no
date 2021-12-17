dca8121dec946cb0ab362a5ee4ae4aa1f5814a1c
Daniel Jackson <daniel@snilldesign.no>
2021-12-17 11:41:31 +0100

    Service for finding duplicates of a space

--- app/services/spaces/duplicate_detector_service.rb
+++ app/services/spaces/duplicate_detector_service.rb
@@ -11,14 +11,27 @@ module Spaces
     end
 
     def call
-      result = find_duplicates
-      return [] if result.nil?
+      potential_duplicates = find_potential_duplicates
+      return potential_duplicates if potential_duplicates.present?
 
-      result
+      nil
     end
 
-    def find_duplicates
-      []
+    def find_potential_duplicates
+      duplicates = []
+      duplicates << Space.find_by(title: @title) if @title.present?
+      duplicates << Space.find_by(full_address) if full_address.present?
+      duplicates.compact
+    end
+
+    def full_address
+      return nil unless @address.present? && @post_number.present? && @post_address.present?
+
+      {
+        address: @address,
+        post_number: @post_number,
+        post_address: @post_address
+      }
     end
   end
 end
