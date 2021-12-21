271c27f95eae4a630b4c730436786e6a06e2cd15
Daniel Jackson <daniel@snilldesign.no>
2021-12-21 17:05:39 +0100

    Stop and enable form submit, and refactor to put helper functions at the bottom

--- app/javascript/controllers/duplicate_checker_controller.js
+++ app/javascript/controllers/duplicate_checker_controller.js
@@ -30,40 +30,6 @@ export default class extends Controller {
     });
   }
 
-  hideRestOfForm() {
-    this.duplicatesRenderHereTarget.classList.remove("hidden")
-    this.hiddenUntilCheckedTarget.classList.add("hidden")
-  }
-
-  showRestOfForm() {
-    this.duplicatesRenderHereTarget.classList.add("hidden")
-    this.hiddenUntilCheckedTarget.classList.remove("hidden")
-  }
-
-  ignoreDuplicates() {
-    this.showDuplicatesButtonTarget.classList.remove("hidden")
-    this.ignoreDuplicatesButtonTarget.classList.add("hidden")
-    this.hiddenUntilCheckedTarget.classList.remove("hidden")
-    this.duplicatesTarget.classList.add("hidden")
-
-  }
-
-  unIgnoreDuplicates() {
-    this.showDuplicatesButtonTarget.classList.add("hidden")
-    this.ignoreDuplicatesButtonTarget.classList.remove("hidden")
-    this.duplicatesTarget.classList.remove("hidden")
-  }
-
-  checkIfDataIsStale(title, address, post_number) {
-    if (title !== this.state.title || address !== this.state.address || post_number !== this.state.post_number) {
-      this.state.title = title
-      this.state.address = address
-      this.state.post_number = post_number
-      return false
-    }
-    return true
-  }
-
   async checkDuplicates() {
     const data = new FormData(this.element);
     const address = data.get("space[address]");
@@ -89,4 +55,56 @@ export default class extends Controller {
 
     return this.showRestOfForm()
   }
+
+  checkIfDataIsStale(title, address, post_number) {
+    if (title !== this.state.title || address !== this.state.address || post_number !== this.state.post_number) {
+      this.state.title = title
+      this.state.address = address
+      this.state.post_number = post_number
+      return false
+    }
+    return true
+  }
+
+  hideRestOfForm() {
+    this.stopFormSubmit()
+    show(this.duplicatesRenderHereTarget)
+    hide(this.hiddenUntilCheckedTarget)
+  }
+
+  showRestOfForm() {
+    this.enableFormSubmit()
+    hide(this.duplicatesRenderHereTarget)
+    show(this.hiddenUntilCheckedTarget)
+  }
+
+  ignoreDuplicates() {
+    show(this.showDuplicatesButtonTarget)
+    show(this.hiddenUntilCheckedTarget)
+    hide(this.ignoreDuplicatesButtonTarget)
+    hide(this.duplicatesTarget)
+  }
+
+  unIgnoreDuplicates() {
+    hide(this.showDuplicatesButtonTarget)
+    show(this.ignoreDuplicatesButtonTarget)
+    show(this.duplicatesTarget)
+  }
+
+  stopFormSubmit() {
+    this.element.querySelector("input[type=submit]").disabled = true
+  }
+
+  enableFormSubmit() {
+    this.element.querySelector("input[type=submit]").disabled = false
+  }
+
+}
+
+const hide = (element) => {
+  element.classList.add("hidden")
+}
+
+const show = (element) => {
+  element.classList.remove("hidden")
 }
