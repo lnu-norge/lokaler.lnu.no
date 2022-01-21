f2a27c9f6ecb6599fb5eeeaaeba0ad56f68242ee
mathias234 <mathiasda98@gmail.com>
2022-01-21 14:24:24 +0100

    Fix rubocop

--- spec/views/space_groups/edit.html.erb_spec.rb
+++ spec/views/space_groups/edit.html.erb_spec.rb
@@ -9,13 +9,13 @@ RSpec.describe "space_groups/edit", type: :view do
     assign(:space_group, space_group)
     render
 
-    expect(rendered).to match /form/
-    expect(rendered).to match /#{space_group.title}/
-    expect(rendered).to match /space_group_how_to_book/
-    expect(rendered).to match /space_group_terms/
-    expect(rendered).to match /space_group_pricing/
-    expect(rendered).to match /space_group_who_can_use/
-    expect(rendered).to match /space_group_about/
-    expect(rendered).to match /submit/
+    expect(rendered).to match(/form/)
+    expect(rendered).to match(/#{space_group.title}/)
+    expect(rendered).to match(/space_group_how_to_book/)
+    expect(rendered).to match(/space_group_terms/)
+    expect(rendered).to match(/space_group_pricing/)
+    expect(rendered).to match(/space_group_who_can_use/)
+    expect(rendered).to match(/space_group_about/)
+    expect(rendered).to match(/submit/)
   end
 end
