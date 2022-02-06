6aa7ee6badc42adb4b210c0b9c638c57dcef7658
Daniel Jackson <daniel@snilldesign.no>
2022-02-06 13:49:51 +0100

    Add helper functions for modal, and replace space contact modal with new modal helpers

--- app/helpers/modal_helper.rb
+++ app/helpers/modal_helper.rb
@@ -25,6 +25,16 @@ module ModalHelper
     }
   end
 
+  def button_to_modal(modal_id, **options, &block)
+    button_tag **options, data: {
+      controller: "modal",
+      "modal-target": "openButton",
+      "modal-to-toggle": modal_id
+    } do
+      yield block
+    end
+  end
+
   def modal_for(modal_id, &block)
     render partial: "shared/modal", locals: {
       modal_id: modal_id,
--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,19 +2,55 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="modal"
 export default class extends Controller {
-  static targets = [ "modal", "background", "closeButton" ]
+  static targets = [ "wrapper", "background", "modal", "focus", "openButton", "closeButton" ]
 
   connect() {
+    this.connectOpenButton()
+    this.connectBackground()
+  }
+
+  disconnect() {
+    super.disconnect();
+  }
+
+  connectOpenButton() {
+    if (!this.hasOpenButtonTarget) return;
+
+    this.openButtonTarget.onclick = () => this.open()
+  }
+
+  connectBackground() {
+    if (!this.hasBackgroundTarget) return;
+
     this.backgroundTarget.onclick = (event) => {
       // Check that we didn't click anything else:
       if (event.target !== this.backgroundTarget) return
 
       // Then close the modal:
-      this.closeButtonTarget.click()
+      this.close()
     }
   }
 
-  disconnect() {
-    super.disconnect();
+  close() {
+    this.wrapperTarget.classList.toggle("hidden")
+  }
+
+  open() {
+    const idToOpen = this.openButtonTarget.dataset.modalToToggle
+
+    const toggleableElement = document.querySelector(
+        `[data-modal-id="${idToOpen}"]`
+    )
+
+    toggleableElement.classList.toggle("hidden");
+
+    const modalIsOpen = !toggleableElement.classList.contains("hidden");
+    if (!modalIsOpen) return;
+
+    // Focus any elements set to be focused when the modal opens
+    const focusMe = toggleableElement.querySelector("[data-modal-focus]")
+    if (!focusMe) return;
+
+    focusMe.focus();
   }
 }
