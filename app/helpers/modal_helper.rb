ff1a33f1f41221c5179da2f077ba481ff2631b73
Daniel Jackson <daniel@snilldesign.no>
2022-02-06 12:30:51 +0100

    WIP moving turbo modals to own function and adding helper function for hiding and showing elements

--- app/helpers/modal_helper.rb
+++ app/helpers/modal_helper.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 module ModalHelper
-  def modal_link_to(text, path, **options)
+  def turbo_modal_link_to(text, path, **options)
     id = options[:id] || path
     turbo_frame_tag id do
       link_to text, path, **options
     end
   end
 
-  def modal_link_to_with_block(path, **options, &block)
+  def turbo_modal_link_to_with_block(path, **options, &block)
     id = options[:id] || path
     turbo_frame_tag id do
       link_to path, **options do
@@ -17,11 +17,29 @@ module ModalHelper
     end
   end
 
-  def modal_content_for(id, parent, &block)
-    render partial: "shared/modal", locals: {
+  def turbo_modal_content_for(id, parent, &block)
+    render partial: "shared/turbo_modal", locals: {
       id: id,
       parent: parent,
       block: block
+    }
+  end
+
+  def modal_for(modal_id, &block)
+    render partial: "shared/modal", locals: {
+      modal_id: modal_id,
+      block: block
+    }
+  end
+
+  def modal_button_for(text = nil, modal_id = nil, **options)
+    button_tag text, {
+      data: {
+        controller: "modal",
+        action: "click->modal#openModal",
+        modal_id: modal_id
+      },
+      **options
     }
   end
 end
--- app/javascript/controllers/hidden_controller.js
+++ app/javascript/controllers/hidden_controller.js
@@ -1,13 +1,31 @@
 import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="hidden"
+
+// Add the following to a clickable element to
+// toggle the class "hidden" on a target:
+//    data-controller="hidden"
+//    data-hidden-target="toggleButton"
+//    data-hidden-id-to-toggle="UNIQUE_TARGET_ID_STRING"
+//
+// Then add on the target:
+//     data-hidden-id="UNIQUE_TARGET_ID_STRING"
 export default class extends Controller {
-  static targets = ["toggleableElement", "focus"]
+  static targets = ["focus", "toggleButton"]
+
+  connect() {
+    this.toggleButtonTarget.addEventListener("click", () => this.toggle())
+  }
 
   toggle() {
-    this.toggleableElementTarget.classList.toggle("hidden");
-    const hidden = this.toggleableElementTarget.classList.contains("hidden");
-    if (!hidden) {
+    const idToToggle = this.toggleButtonTarget.dataset.hiddenIdToToggle
+    const toggleableElement = document.querySelector(
+        `[data-hidden-id="${idToToggle}"]`
+    )
+    toggleableElement.classList.toggle("hidden");
+
+    const elementIsHidden = toggleableElement.classList.contains("hidden");
+    if (!elementIsHidden && this.focusTarget) {
       this.focusTarget.focus();
     }
   }
