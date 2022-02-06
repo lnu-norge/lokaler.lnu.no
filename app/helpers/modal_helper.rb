0d5b61c524d7748aeb873a8048c7d6e4582fffb8
Daniel Jackson <daniel@snilldesign.no>
2022-02-06 19:45:29 +0100

    Remove old modal system

--- app/helpers/modal_helper.rb
+++ app/helpers/modal_helper.rb
@@ -1,30 +1,6 @@
 # frozen_string_literal: true
 
 module ModalHelper
-  def turbo_modal_link_to(text, path, **options)
-    id = options[:id] || path
-    turbo_frame_tag id do
-      link_to text, path, **options
-    end
-  end
-
-  def turbo_modal_link_to_with_block(path, **options, &block)
-    id = options[:id] || path
-    turbo_frame_tag id do
-      link_to path, **options do
-        yield block
-      end
-    end
-  end
-
-  def turbo_modal_content_for(id, parent, &block)
-    render partial: "shared/turbo_modal", locals: {
-      id: id,
-      parent: parent,
-      block: block
-    }
-  end
-
   def button_to_modal(modal_id, **options, &block)
     button_tag(**options, data: {
                  controller: "modal",
