90db088e83dab6fa96db8853552bc5877cc793c4
Daniel Jackson <daniel@snilldesign.no>
2022-02-06 21:12:27 +0100

    Fix tests so they know to look for the right button

--- spec/features/user_creates_review_spec.rb
+++ spec/features/user_creates_review_spec.rb
@@ -26,13 +26,8 @@ describe "User creates review", js: true do
     login_and_logout_with_warden do
       visit space_path(space)
 
-      first(:link, I18n.t("reviews.add_review")).click
+      click_button(I18n.t("reviews.add_review"))
       click_link(I18n.t("reviews.form.have_you_been_there_answers.been_there"))
-
-      # choose("review_facility_reviews_attributes_#{facility1.id}__experience_was_not_available",
-      #       allow_label_click: true)
-      # choose("review_facility_reviews_attributes_#{facility2.id}__experience_was_allowed", allow_label_click: true)
-      # find("span", text: I18n.t("multistep_form_navigation.next")).click
 
       choose("review_star_rating_1", allow_label_click: true)
 
@@ -50,8 +45,6 @@ describe "User creates review", js: true do
 
       expect(page).to have_content("Review Title!")
       expect(page).to have_content("Review Comment!")
-      # expect(page).to have_content(facility1.title)
-      # expect(page).to have_content(facility2.title)
       expect(page).to have_content("5000 kr")
     end
   end
@@ -60,26 +53,17 @@ describe "User creates review", js: true do
     login_and_logout_with_warden do
       visit space_path(space)
 
-      first(:link, I18n.t("reviews.add_review")).click
+      click_button(I18n.t("reviews.add_review"))
       click_link(I18n.t("reviews.form.have_you_been_there_answers.not_allowed_to_use"))
 
       fill_in("review_title", with: "Review Title!")
       fill_in("review_comment", with: "Review Comment!")
-
-      # find("span", text: I18n.t("multistep_form_navigation.next")).click
-
-      # choose("review_facility_reviews_attributes_#{facility1.id}__experience_was_not_available",
-      #        allow_label_click: true)
-      # choose("review_facility_reviews_attributes_#{facility2.id}__experience_was_allowed",
-      #        allow_label_click: true)
 
       click_button(I18n.t("multistep_form_navigation.save"))
       expect(page).to have_content(I18n.t("activerecord.attributes.review.not_allowed_to_use"))
 
       expect(page).to have_content("Review Title!")
       expect(page).to have_content("Review Comment!")
-      # expect(page).to have_content(facility1.title)
-      # expect(page).to have_content(facility2.title)
     end
   end
 
@@ -87,20 +71,12 @@ describe "User creates review", js: true do
     login_and_logout_with_warden do
       visit space_path(space)
 
-      first(:link, I18n.t("reviews.add_review")).click
+      click_button(I18n.t("reviews.add_review"))
       click_link(I18n.t("reviews.form.have_you_been_there_answers.only_contacted"))
-
-      #     choose("review_facility_reviews_attributes_#{facility1.id}__experience_was_not_available",
-      #       allow_label_click: true)
-
-      # choose("review_facility_reviews_attributes_#{facility2.id}__experience_was_allowed",
-      #        allow_label_click: true)
 
       click_button(I18n.t("multistep_form_navigation.save"))
 
       expect(page).to have_content(I18n.t("activerecord.attributes.review.only_contacted"))
-      # expect(page).to have_content(facility1.title)
-      # expect(page).to have_content(facility2.title)
     end
   end
 end
