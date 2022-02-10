f4cd66ebc9718e6a0bd96d465454f6981c3d51c1
Daniel Jackson <daniel@snilldesign.no>
2022-02-10 08:55:13 +0100

    Show and hide the form elements depending on typeOfConntact

--- app/javascript/controllers/review_form_controller.js
+++ app/javascript/controllers/review_form_controller.js
@@ -2,7 +2,84 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="new-review"
 export default class extends Controller {
+  static targets = [
+      'typeOfContact',
+      'fullForm',
+      'organizationForm',
+      'moreAboutYourStayForm',
+      'starRatingForm',
+      'textBeenThereForm'
+  ]
+
   connect() {
-    console.log('hi')
+    console.log("hi", this.typeOfContactTargets)
+    this.connectTypeOfContact()
   }
+
+  connectTypeOfContact() {
+    const radios = this.typeOfContactTargets
+
+    // Set up change handlers:
+    radios.forEach(radio => radio.onclick = (e) => {
+        this.typeOfContactChangedTo(e.target.value)
+    })
+
+    // If any are checked already, show the correct form:
+    radios.forEach(radio => {
+      if (radio.checked) {
+        this.typeOfContactChangedTo(radio.value)
+      }
+    })
+  }
+
+  typeOfContactChangedTo(typeOfContact) {
+    switch(typeOfContact) {
+      case "been_there":
+        this.showBeenThereForm()
+        break;
+      case "not_allowed_to_use":
+        this.showNotAllowedToUseForm()
+        break;
+      case "only_contacted":
+        this.showOnlyContactedForm()
+        break;
+      default:
+        throw new Error("Unhandled typeOfContact, add to review form controller")
+    }
+
+    this.showForm()
+  }
+
+  showForm() {
+    this.fullFormTarget.classList.remove('hidden')
+  }
+
+  showBeenThereForm() {
+    show(this.organizationFormTarget)
+    show(this.moreAboutYourStayFormTarget)
+    show(this.starRatingFormTarget)
+    show(this.textBeenThereFormTarget)
+  }
+
+  showNotAllowedToUseForm() {
+    show(this.organizationFormTarget)
+    hide(this.moreAboutYourStayFormTarget)
+    hide(this.starRatingFormTarget)
+    show(this.textBeenThereFormTarget)
+  }
+
+  showOnlyContactedForm() {
+    show(this.organizationFormTarget)
+    hide(this.moreAboutYourStayFormTarget)
+    hide(this.starRatingFormTarget)
+    show(this.textBeenThereFormTarget)
+  }
+}
+
+function show(element) {
+  element.classList.remove('hidden')
+}
+
+function hide(element) {
+  element.classList.add('hidden')
 }
