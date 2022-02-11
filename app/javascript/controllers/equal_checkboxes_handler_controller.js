9f8936ee106af28d9f9846e03818dc7bf517ad8e
Daniel Jackson <daniel@snilldesign.no>
2022-02-11 13:10:09 +0100

    Handle checkboxes and radios for equal fields handler

--- app/javascript/controllers/equal_checkboxes_handler_controller.js
+++ app/javascript/controllers/equal_checkboxes_handler_controller.js
@@ -8,14 +8,84 @@ export default class extends Controller {
   static targets = ['form']
 
   connect() {
-    const checkboxes = this.formTarget.querySelectorAll('input[type=checkbox]')
+    this.handleDuplicateCheckboxesAndRadios()
+    this.handleDuplicateTextInputs()
+  }
 
-    checkboxes.forEach(checkbox => {
-      checkbox.onclick = (e) => {
-        const clicked_box = e.target
-        const same_id = this.formTarget.querySelectorAll(`input[id=${clicked_box.id}]`)
-        same_id.forEach(checkbox => checkbox.checked = clicked_box.checked)
-      }
+  handleDuplicateTextInputs() {
+    // First we get all input fields (no support for textareas yet)
+    const input_fields = [
+      ...this.formTarget.querySelectorAll('input[type=text]')
+    ]
+
+    // Then we get duplicates, grouped by id:
+    const grouped_duplicates = this.group_duplicates(input_fields)
+
+    grouped_duplicates.forEach((group) => {
+      group.forEach((field, index) => {
+        // Equalize all checked values:
+        // TODO: HANDLE FOR INPUTS
+      })
     })
+
+  }
+
+  handleDuplicateCheckboxesAndRadios() {
+    // First we get all checkboxes and radio buttons (no support for input fields yet)
+    const input_fields = [
+      ...this.formTarget.querySelectorAll('input[type=checkbox]'),
+      ...this.formTarget.querySelectorAll('input[type=radio]'),
+    ]
+
+    // Then we get duplicates, grouped by id:
+    const grouped_duplicates = this.group_duplicates(input_fields)
+
+    // Then we handle everything:
+    grouped_duplicates.forEach((group) => {
+      group.forEach((field, index) => {
+        // Equalize all checked values:
+        field.checked = group[group.length - 1].checked
+
+        // Set a click handler:
+        field.onclick = () => {
+          group.forEach(duplicate => duplicate.checked = field.checked)
+        }
+
+        // Only do the rest for the duplicates:
+        if (index === 0) return
+
+        // Add unique ids:
+        field.dataset.original_id = field.id
+        field.id = `${field.id}-${index}`
+
+        // Update labels with correct for
+        const label = field.parentElement.querySelector(`label[for=${field.dataset.original_id}]`)
+        label.setAttribute('for', field.id)
+
+        // Set a fake namm:
+        field.setAttribute('name', `duplicate_${index}_of_${field.name}`)
+
+        // Then check again, as it's lost when name is set:
+        field.checked = group[0].checked
+      })
+    })
+  }
+
+  group_duplicates(input_fields) {
+    // Pretty sure this could be handled with a .reduce or something...
+    const id_count = {}
+    input_fields.forEach(input => {
+      id_count[input.id] = id_count[input.id] ? 1 + id_count[input.id] : 1
+    })
+
+    const duplicate_input_fields = input_fields.filter(input => id_count[input.id] > 1 )
+
+    const grouped_duplicates = duplicate_input_fields.reduce((group, field) => {
+      group[field.id] = group[field.id] ?  [...group[field.id], field] : [field]
+      return group
+    }, {})
+
+    return Object.values(grouped_duplicates)
+    // [ [field, field], [field, field] ... ]
   }
 }
