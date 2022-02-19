b215665390e3722541f1682b3aa43548ebf39da3
Daniel Jackson <daniel@snilldesign.no>
2022-02-19 03:38:15 +0100

    Only aggregate facilities that change when changing facilities

--- app/controllers/facility_reviews_controller.rb
+++ app/controllers/facility_reviews_controller.rb
@@ -19,18 +19,17 @@ class FacilityReviewsController < BaseControllers::AuthenticateController
 
   def create
     @space = Space.find(params["space_id"])
-    parsed = parse_facility_reviews(params["facility_reviews"]["reviews"])
+    affected_facility_ids = []
+
+    parsed = parse_facility_reviews(params["facility_reviews"]["reviews"], affected_facility_ids)
 
     unless FacilityReview.create(parsed)
       render :new, status: :unprocessable_entity
       return
     end
 
-    # TODO: Aggregate only the changed or new facilities, not all
-    # That will require no longer deleting all in parse_facility_reviews, and thus updating,
-    # not creating, in FaclityReview.create. Otherwise, all you need to do is pass a facilities: [Faciliy]
-    # array to aggregate_facility_reviews.
-    @space.aggregate_facility_reviews
+    affected_facilities = Facility.where(id: affected_facility_ids.uniq)
+    @space.aggregate_facility_reviews(facilities: affected_facilities)
 
     update_space_facilities
 
@@ -68,29 +67,47 @@ class FacilityReviewsController < BaseControllers::AuthenticateController
 
   private
 
-  def parse_facility_reviews(facility_reviews)
+  def parse_facility_reviews(raw_data, affected_facility_ids)
     # Converting to .values exists solely because I didn't manage to create a
     # form with radio buttons that sent the facility_reviews in a way that could
     # be automatically picked up by Rails AND still be seen as individual in the form itself.
     # Thus, we have to parse what is sent from the form here into something
     # that more closely resembles what the model expects:
     # Changes welcome!
-    facility_review_values = facility_reviews.values
+    facility_reviews = raw_data.values
 
-    delete_all_previous_reviews facility_review_values
+    handle_changed(facility_reviews, affected_facility_ids)
   end
 
-  # This should be done in the model, but I never figured out how.
-  # I tried to .destroy and filter on before_validation, but
-  # it only got roll-backed.
-  def delete_all_previous_reviews(facility_reviews)
-    ids = facility_reviews.filter_map do |review|
-      review["id"] unless review["id"].empty?
+  # Does two things:
+  # 1. Destroys any existing reviews that are changed, and returns an array of new reviews to be created
+  # 2. And mutates affected_facilities so we can aggregate facilities for those
+  def handle_changed(facility_reviews, affected_facility_ids) # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+    facility_reviews.filter_map do |review|
+      # We minimum need these to do anything useful
+      next unless review[:user_id].present? && review[:facility_id].present? && review[:space_id].present?
+
+      existing_review = FacilityReview.find_by(user_id: review[:user_id],
+                                               facility_id: review[:facility_id],
+                                               space_id: review[:space_id])
+
+      # NB: Unknown experiences do not crate reviews. Thus existing_review.blank? is the same as
+      # experience == "unknown"
+      nothing_changed = (existing_review.blank? && review["experience"] == "unknown") ||
+                        (existing_review.present? && existing_review[:experience] == review["experience"])
+
+      next if nothing_changed
+
+      # Something changed, so add facility to affected facilities
+      affected_facility_ids << review["facility_id"]
+
+      # Destroy the old review, so we are ready to return a new review for creation.
+      existing_review.destroy if existing_review.present?
+
+      # Unknown experiences do not crate reviews.
+      next if review["experience"] == "unknown"
+
+      review
     end
-
-    FacilityReview.where(id: ids).destroy_all
-
-    facility_reviews.delete_if { |review| review["experience"] == "unknown" }
-    facility_reviews
   end
 end
