6b7026d02f0865b9a10c4042e988e26f2d2aa04d
Daniel Jackson <daniel@snilldesign.no>
2022-12-10 12:46:22 +0100

    Remove unnessary .js from require in js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,7 +14,7 @@ ActiveStorage.start()
 import "stylesheets/application"
 import "controllers"
 
-require("../custom/posthog.js")
+require("../custom/posthog")
 
 require("../custom/custom_trix")
 require("@rails/actiontext")
