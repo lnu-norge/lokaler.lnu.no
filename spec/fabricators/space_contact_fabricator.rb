234c09024832a939bd10612e3881c440a6161ede
Daniel Jackson <daniel@snilldesign.no>
2024-01-16 11:45:47 +0100

    Upgrade devise and other non-rails gems, and fix errors that caused

--- spec/fabricators/space_contact_fabricator.rb
+++ spec/fabricators/space_contact_fabricator.rb
@@ -5,7 +5,7 @@ Fabricator(:space_contact) do
   telephone { "40482128" }
   telephone_opening_hours { "Open from: #{opening_hours(10, 5)} to #{opening_hours(4, 0)}" }
   email { Faker::Internet.email }
-  url { Faker::Internet.url }
+  url { Faker::Internet.url host: "example.com" }
   description { Faker::Lorem.sentence(word_count: 2, random_words_to_add: 3) }
   priority { Faker::Number.between(from: 1, to: 5) }
   space
