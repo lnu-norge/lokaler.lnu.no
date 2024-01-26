93430765d281c5c216ed063beeea46a24c1b3a6c
Daniel Jackson <daniel@snilldesign.no>
2024-01-26 10:24:56 +0100

    Add extensions to postcss and fix some minor styling errors from upgrading

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   plugins: {
-    'postcss-import': {},
-    'tailwindcss/nesting': {},
-    tailwindcss: {},
+    'postcss-import': {}, // Allows @import on top of css files
+    'tailwindcss/nesting': {}, // Allows scss style nesting, e.g. h2 { xxx .fancy { yyy }  } will compile to h2 { xxx }; h2.fancy { yyy };
+    'postcss-extend-rule': {}, // Allows scss style extending with @extend
+    tailwindcss: {}, // Allows tailwind
     'postcss-url': {
-      url: 'inline',
+      url: 'inline', // Converts all css images to base64
     },
-    autoprefixer: {},
+    autoprefixer: {}, // Adds vendor prefixes to stuff
   },
 }
