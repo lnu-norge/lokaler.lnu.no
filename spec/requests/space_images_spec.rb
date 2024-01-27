03a12a270ae58a3ccc1ed4287d3b16483b5a76f1
Daniel Jackson <daniel@snilldesign.no>
2024-01-27 20:38:17 +0100

    Create request test for space images. Unfortunately this one passes too...

---
+++ spec/requests/space_images_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "SpaceImages", type: :request do
+  let(:user) { Fabricate(:user) }
+
+  before do
+    sign_in user
+  end
+
+  describe "POST /spaces/:id/upload_image" do
+    let(:space) { Fabricate(:space) }
+    let(:image_file) { fixture_file_upload(TestImageHelper.image_path, TestImageHelper.content_type) }
+
+    it "uploads an image and redirects" do
+      expect do
+        post spaces_upload_image_path(id: space.id), params: { image: [image_file] }
+      end.to change(space.images, :count).by(1)
+      expect(response).to redirect_to(space_path(space.id))
+      follow_redirect!
+      expect(response.body).to include(I18n.t("images.upload_success"))
+    end
+  end
+
+  describe "PUT /spaces/:id/images/:image_id" do
+    let(:space) { Fabricate(:space) }
+    let(:image) { Fabricate(:image, space:) }
+    let(:params) { { image: { caption: "New Caption", credits: "New Credits" } } }
+
+    it "updates the image and redirects" do
+      put(space_image_path(id: space.id, image_id: image.id), params:)
+      expect(response).to redirect_to(space_image_path(space))
+      follow_redirect!
+      expect(response.body).to include(I18n.t("images.update_success"))
+    end
+  end
+
+  describe "DELETE /spaces/:id/images/:image_id" do
+    let(:space) { Fabricate(:space) }
+    let!(:image) { Fabricate(:image, space:) }
+
+    it "deletes the image and redirects" do
+      expect { delete space_image_path(id: space.id, image:) }.to change(Image, :count).by(-1)
+      expect(response).to redirect_to(space_image_path(space))
+      follow_redirect!
+      expect(response.body).to include(I18n.t("images.delete_success"))
+    end
+  end
+end
--- spec/support/test_image/test_image_helper.rb
+++ spec/support/test_image/test_image_helper.rb
@@ -5,6 +5,14 @@ module TestImageHelper
     Rails.root.join("spec/support/test_image/test_image.jpg")
   end
 
+  def self.content_type
+    "image/jpeg"
+  end
+
+  def self.filename
+    "test_image.jpg"
+  end
+
   def self.image_file
     File.open(image_path)
   end
@@ -12,8 +20,8 @@ module TestImageHelper
   def self.image_upload
     {
       io: image_file,
-      filename: "test_image.jpg",
-      content_type: "image/jpeg"
+      filename:,
+      content_type:
     }
   end
 end
