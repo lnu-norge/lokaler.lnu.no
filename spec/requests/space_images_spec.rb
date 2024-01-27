bb40f1024ba5310a6e559f66aa2d5260ee71fe72
Daniel Jackson <daniel@snilldesign.no>
2024-01-27 22:01:38 +0100

    Create failing test for the bug: It uploads an empty string as part of the image set. That string needs to be discarded.

--- spec/requests/space_images_spec.rb
+++ spec/requests/space_images_spec.rb
@@ -13,9 +13,19 @@ RSpec.describe "SpaceImages", type: :request do
     let(:space) { Fabricate(:space) }
     let(:image_file) { fixture_file_upload(TestImageHelper.image_path, TestImageHelper.content_type) }
 
+    it "fails to upload an empty image" do
+      expect do
+        post spaces_upload_image_path(id: space.id), params: { image: [""] }
+      end.not_to change(space.images, :count)
+      expect(response).to redirect_to(space_path(space.id))
+      follow_redirect!
+      expect(response.body).to include(I18n.t("images.upload_failed"))
+    end
+
     it "uploads an image and redirects" do
       expect do
-        post spaces_upload_image_path(id: space.id), params: { image: [image_file] }
+        # Rails for some reason adds an empty "" to the start of the array when allowing multiple uploads
+        post spaces_upload_image_path(id: space.id), params: { image: ["", image_file] }
       end.to change(space.images, :count).by(1)
       expect(response).to redirect_to(space_path(space.id))
       follow_redirect!
@@ -28,7 +38,7 @@ RSpec.describe "SpaceImages", type: :request do
     let(:image) { Fabricate(:image, space:) }
     let(:params) { { image: { caption: "New Caption", credits: "New Credits" } } }
 
-    it "updates the image and redirects" do
+    it "updates the image info and redirects" do
       put(space_image_path(id: space.id, image_id: image.id), params:)
       expect(response).to redirect_to(space_image_path(space))
       follow_redirect!
