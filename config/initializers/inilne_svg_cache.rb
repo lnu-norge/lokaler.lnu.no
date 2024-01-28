7bd0aef5c8199cec3d9e1935331531f8e28cbcf1
Daniel Jackson <daniel@snilldesign.no>
2024-01-28 13:36:19 +0100

    Cache precompiled inline svgs, so they work with assets.compile = false in prod

---
+++ config/initializers/inilne_svg_cache.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# This caches all SVGs in assets folder, and solves inline svg for
+# production use.
+
+InlineSvg.configure do |config|
+  # Only precompile if we aren't compiling on the fly
+  return if Rails.application.config.assets.compile
+
+  assets_path = Rails.public_path.join("assets").to_s
+  return unless Dir.exist? assets_path
+
+  config.asset_file = InlineSvg::CachedAssetFile.new(
+    paths: [
+      assets_path
+    ],
+    filters: /\.svg/
+  )
+end
