873dd532b9c69f5c36979a36962a7d04f185180a
Daniel Jackson <daniel@snilldesign.no>
2024-01-28 14:57:36 +0100

    Run tests by default with compiled assets, to be as close to prod as possible

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,6 +18,9 @@ Rails.application.configure do # rubocop:disable Metrics/BlockLength
     config.serve_static_files = true
   end
 
+  # If we precompile in dev, we want it in it's own folder - not mixed with test or prod assets
+  config.assets.prefix = "/assets-dev"
+
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
