fa5df2f8dfc8d1f8b5a75f8ba9d774aa0b5704ff
Daniel Jackson <daniel@snilldesign.no>
2024-01-28 14:58:05 +0100

    Fix InlineSVG cacher so it adapts to the assets folder for the current enviroment

--- config/initializers/inilne_svg_cache.rb
+++ config/initializers/inilne_svg_cache.rb
@@ -7,7 +7,10 @@ InlineSvg.configure do |config|
   # Only precompile if we aren't compiling on the fly
   break if Rails.application.config.assets.compile
 
-  assets_path = Rails.public_path.join("assets").to_s
+  # Ensure the assets prefix does not start with a slash for correct path joining
+  assets_prefix = Rails.application.config.assets.prefix.to_s.delete_prefix("/")
+  assets_path = Rails.public_path.join(assets_prefix).to_s
+
   break unless Dir.exist? assets_path
 
   config.asset_file = InlineSvg::CachedAssetFile.new(
