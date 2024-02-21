5f89b404bf1be24dba424d3fac176cf94a68be6a
Daniel Jackson <daniel@snilldesign.no>
2024-02-21 18:46:10 +0100

    Add simple text search

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -1,8 +1,23 @@
 import mapboxgl from 'mapbox-gl';
 import { Controller } from "@hotwired/stimulus";
 
+const capsuleHtml = (title) => `<button
+        data-action="click->mapbox#disableCapsule"
+        class="bg-white px-2.5 py-0.5 mt-2 rounded-full border border-gray-200 hover:border-lnu-pink whitespace-nowrap">
+          ${title}
+        </button>`
+
 export default class extends Controller {
-  static targets = [ "facility", "spaceType", "location", "searchBox", "form", "filterCapsules", "searchArea" ]
+  static targets = [
+      "facility",
+      "spaceType",
+      "location",
+      "searchBox",
+      "title",
+      "form",
+      "filterCapsules",
+      "searchArea"
+  ]
 
   async initialize() {
     mapboxgl.accessToken = this.element.dataset.apiKey;
@@ -60,11 +75,7 @@ export default class extends Controller {
   }
 
   updateFilterCapsules() {
-    const capsuleHtml = (title) => `<button
-        data-action="click->mapbox#disableCapsule"
-        class="bg-white px-2.5 py-0.5 mt-2 rounded-full border border-gray-200 hover:border-lnu-pink whitespace-nowrap">
-          ${title}
-        </button>`
+    const titleCapsule = this.titleTarget.value ? capsuleHtml(`"${this.titleTarget.value}"`) : '';
 
     const facilityCapsules = this.facilityTargets.map(t =>
       t.checked ? capsuleHtml(t.id) : ''
@@ -74,21 +85,30 @@ export default class extends Controller {
       t.checked ? capsuleHtml(t.id) : ''
     ).join('');
 
-    this.filterCapsulesTarget.innerHTML = facilityCapsules + spaceTypeCapsules;
+    this.filterCapsulesTarget.innerHTML = titleCapsule + facilityCapsules + spaceTypeCapsules;
   }
 
   disableCapsule(event) {
+    let foundFilterToReset = false;
+
     this.facilityTargets.forEach(t => {
       if (t.id === event.target.innerText) {
         t.checked = false;
+        foundFilterToReset = true;
       }
     });
 
     this.spaceTypeTargets.forEach(t => {
       if (t.id === event.target.innerText) {
         t.checked = false;
+        foundFilterToReset = true;
       }
     });
+
+    if (!foundFilterToReset)  {
+      // Then it's the title serach capsule
+      this.titleTarget.value = '';
+    }
 
     this.updateFilterCapsules();
     this.loadNewMapPosition();
@@ -101,10 +121,12 @@ export default class extends Controller {
     const selectedFacilities = this.selectedFacilities();
     const selectedSpaceTypes = this.selectedSpaceTypes();
     const selectedLocation = this.selectedLocation();
+    const searchForTitle = this.titleTarget.value;
 
     this.setOrDeleteToUrl('selectedFacilities', selectedFacilities);
     this.setOrDeleteToUrl('selectedSpaceTypes', selectedSpaceTypes);
     this.setOrDeleteToUrl('selectedLocation', selectedLocation);
+    this.setOrDeleteToUrl('searchForTitle', searchForTitle);
   }
 
   setOrDeleteToUrl(key, value) {
@@ -137,9 +159,11 @@ export default class extends Controller {
     const selectedFacilities = url.searchParams.get('selectedFacilities');
     const selectedSpaceTypes = url.searchParams.get('selectedSpaceTypes');
     const selectedLocation = url.searchParams.get('selectedLocation');
+    const searchForTitle = url.searchParams.get('searchForTitle');
 
     this.parseSelectedFacilities(selectedFacilities);
     this.parseSelectedSpaceTypes(selectedSpaceTypes);
+    this.parseSearchForTitle(searchForTitle);
 
     this.updateFilterCapsules();
 
@@ -157,6 +181,13 @@ export default class extends Controller {
     }
   }
 
+  parseSearchForTitle(searchForTitle) {
+    if(searchForTitle) {
+      this.titleTarget.value = searchForTitle;
+    } else {
+      this.titleTarget.value = '';
+    }
+  }
   parseSelectedFacilities(selectedFacilities) {
     if(!selectedFacilities) return;
 
@@ -204,6 +235,12 @@ export default class extends Controller {
       }
     });
 
+    this.titleTarget.oninput = () => {
+      this.updateFilterCapsules();
+      this.loadNewMapPosition();
+      this.updateUrl();
+    }
+
     this.spaceTypeTargets.forEach(spaceType => {
       spaceType.onchange = () => {
         this.updateFilterCapsules();
@@ -227,6 +264,7 @@ export default class extends Controller {
     this.formTarget.onsubmit = (event) => {
       // To stop the form from submitting, as that currently does nothing but refresh the page.
       event.preventDefault()
+      this.submitSearch(event)
     };
   }
 
@@ -287,12 +325,15 @@ export default class extends Controller {
       t.checked ? `space_types[]=${encodeURIComponent(t.name)}&` : ''
     ).join('');
 
+    const title = this.titleTarget.value;
+
     return [
       '/spaces_search?',
       `north_west_lat=${northWest.lat}&`,
       `north_west_lng=${northWest.lng}&`,
       `south_east_lat=${southEast.lat}&`,
       `south_east_lng=${southEast.lng}&`,
+      `search_for_title=${title}&`,
       facilitiesString,
       spaceTypesString,
     ].join('');
