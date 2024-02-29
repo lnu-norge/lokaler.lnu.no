219e4096fc0190de8a1780a3016466045791d039
Daniel Jackson <daniel@snilldesign.no>
2024-02-29 10:12:06 +0100

    Get locations from geonorge based on kommuner and fylker, not by seraching by name

--- app/javascript/controllers/locationsearch_controller.js
+++ app/javascript/controllers/locationsearch_controller.js
@@ -10,12 +10,11 @@ export default class extends Controller {
   connect() {
     this.tomselect = new TomSelect('#locationInput', {
           maxOptions: null,
-          valueField: 'kommune',
+          valueField: 'value',
           labelField: 'kommune',
           searchField: ['kommune', 'fylke'],
           optgroupField: 'fylke',
           sortField: ['fylke', 'kommune'],
-          allowEmptyOption: true,
           persist: false, // but do not store user inputted options as dropdown items
           plugins: ['clear_button'], // For multiple select
           optgroups: this.optGroups,
@@ -29,19 +28,26 @@ export default class extends Controller {
   }
 
   get options() {
-    console.log(fylkerAndKommuner)
     return fylkerAndKommuner.map(fylke => {
-      if (!fylke.kommuner) return {
-        kommune: fylke.name,
+      const options = []
+
+      options.push({
+        kommune: `Hele ${fylke.name}`,
+        value: fylke.id,
         fylke: fylke.name
+      })
+
+      if (fylke.kommuner) {
+        fylke.kommuner.forEach(kommune => {
+          options.push({
+            kommune: kommune.name,
+            value: kommune.id,
+            fylke: fylke.name
+          })
+        })
       }
 
-      return fylke.kommuner.map(kommune => {
-        return {
-          kommune: kommune.name,
-          fylke: fylke.name
-        }
-      })
+      return options
     }).flat()
   }
 }
