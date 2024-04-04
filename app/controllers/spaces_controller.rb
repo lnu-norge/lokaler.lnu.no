7252c459cee65b37cf8435ab068eeb5207b86380
Daniel Jackson <daniel@snilldesign.no>
2024-04-04 11:42:10 +0200

    Extract filterable spaces to reusable module

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class SpacesController < BaseControllers::AuthenticateController # rubocop:disable Metrics/ClassLength
+  include FilterableSpaces
   include DefineGroupedFacilitiesForSpace
 
   def index
@@ -170,30 +171,6 @@ class SpacesController < BaseControllers::AuthenticateController # rubocop:disab
       address: params[:space][:address],
       post_number: params[:space][:post_number]
     ) || {}
-  end
-
-  def filter_spaces(params)
-    space_types = params[:space_types]&.map(&:to_i)
-
-    spaces = Space.includes([:images]).filter_on_location(
-      params[:north_west_lat],
-      params[:north_west_lng],
-      params[:south_east_lat],
-      params[:south_east_lng]
-    )
-
-    spaces = spaces.filter_on_title(params[:search_for_title]) if params[:search_for_title].present?
-    spaces = spaces.filter_on_space_types(space_types) if space_types.present?
-
-    filter_on_facilities_and_return_ordered_spaces(spaces:, params:)
-  end
-
-  def filter_on_facilities_and_return_ordered_spaces(spaces:, params:)
-    facilities = params[:facilities]&.map(&:to_i)
-
-    return spaces.order("star_rating DESC NULLS LAST") if facilities.blank?
-
-    Space.filter_on_facilities(spaces, params[:facilities])
   end
 
   def space_params # rubocop:disable  Metrics/MethodLength
