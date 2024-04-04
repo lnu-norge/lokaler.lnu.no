e13053f5f3d3aecfac3da43829095cbfe5935283
Daniel Jackson <daniel@snilldesign.no>
2024-04-04 12:18:29 +0200

    Make sure filtered spaces module can be used without location filter set

--- app/controllers/concerns/filterable_spaces.rb
+++ app/controllers/concerns/filterable_spaces.rb
@@ -3,27 +3,47 @@
 module FilterableSpaces
   private
 
-  def filter_spaces(params)
-    space_types = params[:space_types]&.map(&:to_i)
+  def filter_spaces
+    @spaces = Space.includes([:images]).order("star_rating DESC NULLS LAST")
 
-    spaces = Space.includes([:images]).filter_on_location(
+    filter_by_location
+    filter_by_title
+    filter_by_space_types
+    filter_and_order_by_facilities
+  end
+
+  def filter_by_title
+    return if params[:search_for_title].blank?
+
+    @spaces = @spaces.filter_on_title(params[:search_for_title])
+  end
+
+  def filter_by_space_types
+    return if params[:space_types].blank?
+
+    space_types = params[:space_types]&.map(&:to_i)
+    @spaces = @spaces.filter_on_space_types(space_types)
+  end
+
+  def location_params_present?
+    params[:north_west_lat].present? && params[:north_west_lng].present? &&
+      params[:south_east_lat].present? && params[:south_east_lng].present?
+  end
+
+  def filter_by_location
+    return unless location_params_present?
+
+    @spaces.filter_on_location(
       params[:north_west_lat],
       params[:north_west_lng],
       params[:south_east_lat],
       params[:south_east_lng]
     )
-
-    spaces = spaces.filter_on_title(params[:search_for_title]) if params[:search_for_title].present?
-    spaces = spaces.filter_on_space_types(space_types) if space_types.present?
-
-    filter_on_facilities_and_return_ordered_spaces(spaces:, params:)
   end
 
-  def filter_on_facilities_and_return_ordered_spaces(spaces:, params:)
-    facilities = params[:facilities]&.map(&:to_i)
+  def filter_and_order_by_facilities
+    return if params[:facilities].blank?
 
-    return spaces.order("star_rating DESC NULLS LAST") if facilities.blank?
-
-    Space.filter_on_facilities(spaces, params[:facilities])
+    @spaces = Space.filter_on_facilities(@spaces, params[:facilities])
   end
 end
