3059cb5bb55db1eccf77fc1f795567fa05449091
Daniel Jackson <daniel@snilldesign.no>
2024-05-22 12:27:35 +0200

    WIP show status and notes on space, if active list is set

--- app/controllers/personal_space_lists_spaces/contact_status_controller.rb
+++ app/controllers/personal_space_lists_spaces/contact_status_controller.rb
@@ -10,7 +10,9 @@ module PersonalSpaceListsSpaces
     def update
       @personal_space_list_space.update(contact_status_params)
       flash.now[:notice] = t("personal_space_lists.contact_status_saved")
-      render :edit, locals: { personal_space_list: @personal_space_list }
+      render partial: "personal_space_lists_spaces/contact_status/form",
+             locals: { personal_space_list_space: @personal_space_list_space,
+                       contact_status_updated: true }
     end
 
     private
