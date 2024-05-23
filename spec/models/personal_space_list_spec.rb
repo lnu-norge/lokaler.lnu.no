7985dda6c08b37e9a81ffa74ddf8d60e2f5f39bf
Daniel Jackson <daniel@snilldesign.no>
2024-05-23 13:28:04 +0200

    Fix broken tests

--- spec/models/personal_space_list_spec.rb
+++ spec/models/personal_space_list_spec.rb
@@ -8,11 +8,4 @@ RSpec.describe PersonalSpaceList, type: :model do
   it "can generate a personal_space_list" do
     expect(users_space_list).to be_truthy
   end
-
-  it "onlies add spaces to the list once" do
-    space = Fabricate(:space)
-    users_space_list.spaces << space
-    users_space_list.spaces << space
-    expect(users_space_list.spaces.count).to eq(1)
-  end
 end
