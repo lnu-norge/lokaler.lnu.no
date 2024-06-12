00b988618244ae91de6ce82ac197dc108e73ad2d
Daniel Jackson <daniel@snilldesign.no>
2024-06-12 09:04:37 +0200

    Allow updating space contacts from table, making sure they have seperate IDs per space

---
+++ app/helpers/space_contact_helper.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module SpaceContactHelper
+  def dom_id_form_for_new_space_contact_for(space)
+    "new_space_contact_for_#{space.id}"
+  end
+
+  def dom_id_button_for_new_space_contact_for(space)
+    "new_space_contact_button_for_#{space.id}"
+  end
+
+  def dom_id_new_space_contact_modal(space)
+    "new_space_contact_modal_for_#{space.id}"
+  end
+
+  def dom_id_for_space_contacts_stream(space)
+    "space_contacts_for_space_#{space.id}"
+  end
+end
