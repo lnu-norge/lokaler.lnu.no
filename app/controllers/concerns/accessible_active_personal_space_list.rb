d0495bd85470612006bed16ecc82e4ec7045c000
Daniel Jackson <daniel@snilldesign.no>
2024-09-20 15:12:42 +0200

    More performant search

--- app/controllers/concerns/accessible_active_personal_space_list.rb
+++ app/controllers/concerns/accessible_active_personal_space_list.rb
@@ -6,8 +6,16 @@ module AccessibleActivePersonalSpaceList
   private
 
   def access_active_personal_list
-    @active_personal_space_list = current_user
-                                  &.active_personal_space_list
-                                  &.personal_space_list
+    my_personal_space_list = current_user
+                             &.active_personal_space_list
+                             &.personal_space_list
+
+    if my_personal_space_list
+      @active_personal_space_list = PersonalSpaceList.includes(:spaces,
+                                                               :this_lists_personal_data_on_spaces)
+                                                     .find(my_personal_space_list.id)
+    else
+      @active_personal_space_list = nil
+    end
   end
 end
