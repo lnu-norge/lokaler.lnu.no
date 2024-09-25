a9b2b122f3f20c78be0bafdb70f0e96a953bb219
Daniel Jackson <daniel@snilldesign.no>
2024-09-25 18:05:34 +0200

    Add pagination

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include Pagy::Backend
+
   before_action :set_paper_trail_whodunnit
   before_action :configure_permitted_parameters, if: :devise_controller?
 
