c1ef9a83f16f77cb609d901ca6b8c1433ba8fedb
Daniel Jackson <daniel@snilldesign.no>
2024-09-27 09:04:33 +0200

    Add PostGIS

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_26_071852) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_27_065943) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "postgis"
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
