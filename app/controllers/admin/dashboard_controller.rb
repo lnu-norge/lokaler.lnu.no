9abf490cd879a8fb85f447fa7f3d47c87531fb97
Daniel Jackson <daniel@snilldesign.no>
2024-11-21 14:08:08 +0100

    Add simple statistics for admins and admin menu

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,91 @@
 
 module Admin
   class DashboardController < BaseControllers::AuthenticateAsAdminController
-    def index; end
+    PERIODS_TO_GROUP_BY = %i[
+      hour
+      day
+      week
+      month
+      year
+    ].freeze
+    def index
+      set_date_range
+      set_period_grouping
+
+      statistics
+    end
+
+    private
+
+    def statistics
+      paper_trail_statistics
+      space_statistics
+      review_statistics
+      facility_review_statistics
+      user_statistics
+      list_statistics
+    end
+
+    def paper_trail_statistics
+      @changes_created = PaperTrail::Version.group_by_period(@period_grouping, :created_at, range: @date_range).count
+      @unique_users_creating_changes = PaperTrail::Version.group_by_period(@period_grouping, :created_at,
+                                                                           range: @date_range).count(:whodunnit)
+      @most_changes_per_user = PaperTrail::Version
+                               .where(created_at: @date_range)
+                               .where.not(whodunnit: nil)
+                               .group(:whodunnit)
+                               .count
+                               .sort_by { |_, count| count }
+                               .reverse
+    end
+
+    def space_statistics
+      @spaces_created = Space.group_by_period(@period_grouping, :created_at, range: @date_range).count
+    end
+
+    def review_statistics
+      @reviews_created = Review.group_by_period(@period_grouping, :created_at, range: @date_range).count
+    end
+
+    def facility_review_statistics
+      @facility_reviews_created = FacilityReview.group_by_period(@period_grouping, :created_at,
+                                                                 range: @date_range).count
+    end
+
+    def user_statistics
+      @users_created = User.group_by_period(@period_grouping, :created_at, range: @date_range).count
+    end
+
+    def list_statistics
+      @lists_created = PersonalSpaceList.group_by_period(@period_grouping, :created_at, range: @date_range).count
+    end
+
+    def set_period_grouping
+      @period_grouping = params.permit("period_grouping")[:period_grouping].presence || default_period_grouping
+    end
+
+    def default_period_grouping
+      days_of_stats_shown = @date_range.count
+
+      case days_of_stats_shown
+      when 0
+        :hour
+      when 1..31
+        :day
+      when 32..200
+        :week
+      when 201..720
+        :month
+      else
+        :year
+      end
+    end
+
+    def set_date_range
+      start_date = params[:start_date]&.to_date || 1.year.ago.to_date
+      end_date = params[:end_date]&.to_date || Date.current
+
+      @date_range = start_date..end_date
+    end
   end
 end
