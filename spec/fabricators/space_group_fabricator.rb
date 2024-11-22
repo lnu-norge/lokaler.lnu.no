4fd057c177b5338045d8b4f7febf4e89354aa30f
Daniel Jackson <daniel@snilldesign.no>
2024-11-22 14:33:55 +0100

    Combine terms, pricing and who_can_use rich text fields into just one: "Terms and pricing"

--- spec/fabricators/space_group_fabricator.rb
+++ spec/fabricators/space_group_fabricator.rb
@@ -4,9 +4,7 @@ Fabricator(:space_group) do
   title Faker::Name.first_name
   about Faker::Lorem.paragraphs.map { |p| "<p>#{p}</p>" }.join
   how_to_book Faker::Lorem.paragraphs.map { |p| "<p>#{p}</p>" }.join
-  terms Faker::Lorem.paragraphs.map { |p| "<p>#{p}</p>" }.join
-  pricing Faker::Lorem.paragraphs.map { |p| "<p>#{p}</p>" }.join
-  who_can_use Faker::Lorem.paragraphs.map { |p| "<p>#{p}</p>" }.join
+  terms_and_pricing Faker::Lorem.paragraphs.map { |p| "<p>#{p}</p>" }.join
 end
 
 # == Schema Information
