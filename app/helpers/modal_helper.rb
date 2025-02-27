700163d8520147253cacb9d8175fe2815a051cdb
Daniel Jackson <daniel@snilldesign.no>
2025-02-27 14:05:29 +0100

    Make sure modal buttons don't trigger form submits by setting type="button" on them'

--- app/helpers/modal_helper.rb
+++ app/helpers/modal_helper.rb
@@ -2,11 +2,14 @@
 
 module ModalHelper
   def button_to_modal(modal_id, **options, &block)
-    button_tag(**options, data: {
-                 controller: "modal",
-                 "modal-target": "openButton",
-                 "modal-to-toggle": modal_id
-               }) do
+    button_tag(
+      type: "button",
+      **options, data: {
+        controller: "modal",
+        "modal-target": "openButton",
+        "modal-to-toggle": modal_id
+      }
+    ) do
       yield block
     end
   end
