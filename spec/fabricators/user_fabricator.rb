21d47a7e24962a7aac966799799927bfea18bd6d
Daniel Jackson <daniel@snilldesign.no>
2025-03-31 13:34:27 +0200

    Add robot users

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -32,6 +32,7 @@ end
 #  remember_token         :string(20)
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  type                   :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #
@@ -39,4 +40,5 @@ end
 #
 #  index_users_on_email                 (email) UNIQUE
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_type                  (type)
 #
