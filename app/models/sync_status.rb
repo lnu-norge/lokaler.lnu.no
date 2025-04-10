ec29be1c7c4d39d4eaed2e66b77a0494e6f8c93d
Daniel Jackson <daniel@snilldesign.no>
2025-04-10 12:50:14 +0200

    Clean up sync status a bit, removing space and last_attempt_was_successful

--- app/models/sync_status.rb
+++ app/models/sync_status.rb
@@ -1,20 +1,14 @@
 # frozen_string_literal: true
 
 class SyncStatus < ApplicationRecord
-  belongs_to :space, optional: true
-
   validates :source, presence: true
   validates :id_from_source, presence: true, uniqueness: { scope: :source }
 
+  scope :successful, -> { where.not(last_successful_sync_at: nil) }
+  scope :failed, -> { where(last_successful_sync_at: nil) }
+
   def self.for(source:, id_from_source:)
     find_or_create_by!(id_from_source:, source:)
-  end
-
-  def self.for_space(space, source:)
-    find_or_initialize_by(space: space, source: source) do |sync_status|
-      sync_status.update(id_from_source: space.id)
-      sync_status.save!
-    end
   end
 
   def log_start
@@ -22,18 +16,33 @@ class SyncStatus < ApplicationRecord
   end
 
   def log_success
-    update(last_successful_sync_at: Time.current, last_attempt_was_successful: true)
-    update(error_message: nil)
+    update(last_successful_sync_at: Time.current)
+    clear_error_messages
   end
 
   def log_failure(error)
-    error_message = error.respond_to?(:message) ? error.message : error.to_s
-    full_error_message = error.respond_to?(:full_message) ? error.full_message : ""
+    update_error_messages_with(error)
 
     Rails.logger.error("Error when syncing #{source}: #{error_message}")
-    update(last_attempt_was_successful: false) if valid?
-    update(error_message:)
-    update(full_error_message:)
+    update(last_successful_sync_at: nil)
+  end
+
+  private
+
+  def clear_error_messages
+    update(
+      error_message: nil,
+      full_error_message: nil
+    )
+  end
+
+  def update_error_messages_with(error)
+    error_message = error.respond_to?(:message) ? error.message : error.to_s
+    full_error_message = error.respond_to?(:full_message) ? error.full_message : ""
+    update(
+      error_message:,
+      full_error_message:
+    )
   end
 end
 
@@ -41,26 +50,18 @@ end
 #
 # Table name: sync_statuses
 #
-#  id                          :bigint           not null, primary key
-#  error_message               :string
-#  full_error_message          :text
-#  id_from_source              :string           not null
-#  last_attempt_was_successful :boolean
-#  last_attempted_sync_at      :datetime
-#  last_successful_sync_at     :datetime
-#  source                      :string           not null
-#  created_at                  :datetime         not null
-#  updated_at                  :datetime         not null
-#  space_id                    :bigint
+#  id                      :bigint           not null, primary key
+#  error_message           :string
+#  full_error_message      :text
+#  id_from_source          :string           not null
+#  last_attempted_sync_at  :datetime
+#  last_successful_sync_at :datetime
+#  source                  :string           not null
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
 #
 # Indexes
 #
 #  index_sync_statuses_on_id_from_source_and_source  (id_from_source,source) UNIQUE
 #  index_sync_statuses_on_source                     (source)
-#  index_sync_statuses_on_space_id                   (space_id)
-#  index_sync_statuses_on_space_id_and_source        (space_id,source) UNIQUE
-#
-# Foreign Keys
-#
-#  fk_rails_...  (space_id => spaces.id)
 #
