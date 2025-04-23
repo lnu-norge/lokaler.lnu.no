2da39473910cf1ada8a1b3cec3bef93e0bba4e20
Daniel Jackson <daniel@snilldesign.no>
2025-04-23 09:46:39 +0200

    Separate out staging enviroment, to turn off recurring tasks (and other production only stuff)

---
+++ config/environments/staging.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Staging should be configured like production, unless overridden below
+require_relative "production"
+
+Rails.application.configure do
+  # Overrides go here
+end
---
+++ db/seeds/staging.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+# Staging should use production seeds
+require "./db/seeds/production"
