a6694c39302118247593a83356c10742ca0f1742
Daniel Jackson <daniel@snilldesign.no>
2025-06-05 10:21:57 +0200

    Contact us button

---
+++ app/helpers/contact_us_helper.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module ContactUsHelper
+  def mailto_contact_us_path
+    "mailto:#{email}?subject=#{ERB::Util.url_encode(subject)}&body=#{ERB::Util.url_encode(body)}"
+  end
+
+  private
+
+  def email
+    "lokaler@lnu.no"
+  end
+
+  def subject
+    "Tilbakemedling om LNU Lokaler"
+  end
+
+  def body
+    "Hei!
+
+Kjapp tilbakemelding om LNU Lokaler:
+
+
+
+Mvh
+#{current_user.name}
+#{current_user.organization_name.presence}
+"
+  end
+end
